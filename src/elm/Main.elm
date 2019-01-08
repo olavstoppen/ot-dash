86b87dea6dec4f800f0e25cf6d94d82f94e5d152
mimarz <michael.marszalek@olavstoppen.no>
2019-01-08 19:08:03 +0100

    Moved update to seperate file

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,4 +1,4 @@
-port module Main exposing (background, getPage, httpErrorToString, init, main, toJs, update, view)
+module Main exposing (background, getPage, init, link, main, sidebar, subscriptions, view)
 
 import Browser exposing (UrlRequest(..))
 import Browser.Navigation as Nav exposing (Key)
@@ -20,50 +20,36 @@ import Services exposing (..)
 import Task
 import Time exposing (..)
 import Tuple exposing (..)
+import Updates exposing (getActivePage, pages, update, urlParser)
 import Url exposing (Url)
 import Url.Parser as UrlParser exposing ((</>), Parser)
 
 
-port toJs : String -> Cmd msg
+
+-- ---------------------------
+-- MAIN
+-- ---------------------------
 
 
-urlParser : Parser (Page -> msg) msg
-urlParser =
-    UrlParser.oneOf
-        [ UrlParser.map Transit <| UrlParser.s "transit"
-        , UrlParser.map Slack <| UrlParser.s "slack"
-        , UrlParser.map Birthday <| UrlParser.s "birthday"
-        , UrlParser.map Instagram <| UrlParser.s "instagram"
-        , UrlParser.map Weather <| UrlParser.s "weather"
-        , UrlParser.map Lunch <| UrlParser.s "lunch"
-        ]
+main : Program Flags Model Msg
+main =
+    Browser.application
+        { init = init
+        , update = update
+        , view =
+            \m ->
+                { title = "Olavstoppen"
+                , body = [ view m ]
+                }
+        , subscriptions = subscriptions
+        , onUrlRequest = OnUrlRequest
+        , onUrlChange = OnUrlChange
+        }
 
 
-staticPages : List Page
-staticPages =
-    [ Transit
-    , Slack
-    , Birthday
-    , Instagram
-    , Weather
-    , Lunch
-    ]
-
-
-getActivePage : Page -> ( Int, Page )
-getActivePage page =
-    List.indexedMap pair staticPages
-        |> List.filter (\( _, page_ ) -> page == page_)
-        |> List.head
-        |> Maybe.withDefault ( 0, Transit )
-
-
-getActivePage_ : Int -> ( Int, Page )
-getActivePage_ index =
-    List.indexedMap pair staticPages
-        |> List.filter (\( index_, _ ) -> index == index_)
-        |> List.head
-        |> Maybe.withDefault ( 0, Transit )
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Time.every 1000 UpdateNow
 
 
 init : Flags -> Url -> Key -> ( Model, Cmd Msg )
@@ -80,7 +66,7 @@ init flags url key =
             , now = millisToPosix 0
             , pageCountdown = 30000
             , activePage = getActivePage urlPage
-            , pages = List.indexedMap pair staticPages
+            , pages = pages
             , publicTransport = []
             , weatherInfo = WeatherInfo (WeatherData (Temperature 0.0 0.0 0.0 0.0) Nothing "" "" "") []
             , birthdays = [ Person "Michael" "Marszalek" "/images/typing.png" ]
@@ -103,136 +89,6 @@ init flags url key =
         , fetchLunchMenu UpdateLunchMenu model
         ]
     )
-
-
-
--- ---------------------------
--- UPDATE
--- ---------------------------
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update message model =
-    case message of
-        SetTimeZone zone ->
-            ( { model | zone = zone }, Cmd.none )
-
-        OnUrlRequest urlRequest ->
-            ( model, handleUrlRequest model.key urlRequest )
-
-        OnUrlChange url ->
-            let
-                urlPage =
-                    Maybe.withDefault (second model.activePage) <| UrlParser.parse urlParser url
-
-                nextPage =
-                    getActivePage urlPage
-            in
-            ( { model | activePage = nextPage }, Cmd.none )
-
-        UpdateNow now ->
-            let
-                nextPageCountdown =
-                    model.pageCountdown - 1
-
-                updatedModel =
-                    { model | now = now }
-            in
-            if nextPageCountdown < 0 then
-                let
-                    nextUrlPage =
-                        first model.activePage
-                            |> (+) 1
-                            |> getActivePage_
-                            |> second
-                in
-                ( { updatedModel | pageCountdown = 30 }, Nav.pushUrl model.key <| getPageKey nextUrlPage )
-
-            else
-                ( { updatedModel | pageCountdown = nextPageCountdown }, Cmd.none )
-
-        UpdateSlackEvents res ->
-            case res of
-                Ok events ->
-                    ( { model | slackEvents = events }, Cmd.none )
-
-                Err err ->
-                    Debug.log (Debug.toString err)
-                        ( model, Cmd.none )
-
-        UpdateWeather res ->
-            case res of
-                Ok weatherInfo ->
-                    ( { model | weatherInfo = weatherInfo }, Cmd.none )
-
-                Err err ->
-                    Debug.log (Debug.toString err)
-                        ( model, Cmd.none )
-
-        UpdateInstagram res ->
-            case res of
-                Ok instagram ->
-                    ( { model | instagram = instagram }, Cmd.none )
-
-                Err err ->
-                    Debug.log (Debug.toString err)
-                        ( model, Cmd.none )
-
-        UpdatePublicTransport res ->
-            case res of
-                Ok publicTransports ->
-                    ( { model | publicTransport = sortWith sortTransport <| List.concat publicTransports }, Cmd.none )
-
-                Err err ->
-                    Debug.log (Debug.toString err)
-                        ( model, Cmd.none )
-
-        UpdateSlackInfo res ->
-            case res of
-                Ok slackInfo ->
-                    ( { model | slackInfo = slackInfo }, Cmd.none )
-
-                Err err ->
-                    Debug.log (Debug.toString err)
-                        ( model, Cmd.none )
-
-        UpdateLunchMenu res ->
-            case res of
-                Ok lunchMenu ->
-                    ( { model | lunchMenu = lunchMenu }, Cmd.none )
-
-                Err err ->
-                    Debug.log (Debug.toString err)
-                        ( model, Cmd.none )
-
-
-handleUrlRequest : Key -> UrlRequest -> Cmd Msg
-handleUrlRequest key urlRequest =
-    case urlRequest of
-        Internal url ->
-            Nav.pushUrl key (Url.toString url)
-
-        External url ->
-            Nav.load url
-
-
-httpErrorToString : Http.Error -> String
-httpErrorToString err =
-    case err of
-        BadUrl _ ->
-            "BadUrl"
-
-        Timeout ->
-            "Timeout"
-
-        NetworkError ->
-            "NetworkError"
-
-        BadStatus _ ->
-            "BadStatus"
-
-        BadBody s ->
-            "BadBody: " ++ s
 
 
 
@@ -303,30 +159,3 @@ sidebar model =
 link : Page -> Page -> String -> String -> Html Msg
 link activePage page href_ title =
     a [ classList [ ( "page", activePage == page ) ], href href_ ] [ text title ]
-
-
-
--- ---------------------------
--- MAIN
--- ---------------------------
-
-
-main : Program Flags Model Msg
-main =
-    Browser.application
-        { init = init
-        , update = update
-        , view =
-            \m ->
-                { title = "Olavstoppen"
-                , body = [ view m ]
-                }
-        , subscriptions = subscriptions
-        , onUrlRequest = OnUrlRequest
-        , onUrlChange = OnUrlChange
-        }
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Time.every 1000 UpdateNow
---
+++ src/elm/Updates.elm
@@ -0,0 +1,198 @@
+port module Updates exposing (getActivePage, pages, update, urlParser)
+
+import Browser exposing (UrlRequest(..))
+import Browser.Navigation as Nav exposing (Key)
+import Helpers exposing (getPageKey, sortTransport)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http exposing (Error(..))
+import Json.Decode as Decode
+import List exposing (..)
+import Model exposing (..)
+import Page.Birthday as Birthday
+import Page.Instagram as Instagram
+import Page.Lunch as Lunch
+import Page.Slack as Slack
+import Page.Transit as Transit
+import Page.Weather as Weather
+import Services exposing (..)
+import Task
+import Time exposing (..)
+import Tuple exposing (..)
+import Url exposing (Url)
+import Url.Parser as UrlParser exposing ((</>), Parser)
+
+
+
+-- ---------------------------
+-- UPDATE
+-- ---------------------------
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update message model =
+    case message of
+        SetTimeZone zone ->
+            ( { model | zone = zone }, Cmd.none )
+
+        OnUrlRequest urlRequest ->
+            ( model, handleUrlRequest model.key urlRequest )
+
+        OnUrlChange url ->
+            let
+                urlPage =
+                    Maybe.withDefault (second model.activePage) <| UrlParser.parse urlParser url
+
+                nextPage =
+                    getActivePage urlPage
+            in
+            ( { model | activePage = nextPage }, Cmd.none )
+
+        UpdateNow now ->
+            let
+                nextPageCountdown =
+                    model.pageCountdown - 1
+
+                updatedModel =
+                    { model | now = now }
+            in
+            if nextPageCountdown < 0 then
+                let
+                    nextUrlPage =
+                        first model.activePage
+                            |> (+) 1
+                            |> getActivePage_
+                            |> second
+                in
+                ( { updatedModel | pageCountdown = 30 }, Nav.pushUrl model.key <| getPageKey nextUrlPage )
+
+            else
+                ( { updatedModel | pageCountdown = nextPageCountdown }, Cmd.none )
+
+        UpdateSlackEvents res ->
+            case res of
+                Ok events ->
+                    ( { model | slackEvents = events }, Cmd.none )
+
+                Err err ->
+                    Debug.log (Debug.toString err)
+                        ( model, Cmd.none )
+
+        UpdateWeather res ->
+            case res of
+                Ok weatherInfo ->
+                    ( { model | weatherInfo = weatherInfo }, Cmd.none )
+
+                Err err ->
+                    Debug.log (Debug.toString err)
+                        ( model, Cmd.none )
+
+        UpdateInstagram res ->
+            case res of
+                Ok instagram ->
+                    ( { model | instagram = instagram }, Cmd.none )
+
+                Err err ->
+                    Debug.log (Debug.toString err)
+                        ( model, Cmd.none )
+
+        UpdatePublicTransport res ->
+            case res of
+                Ok publicTransports ->
+                    ( { model | publicTransport = sortWith sortTransport <| List.concat publicTransports }, Cmd.none )
+
+                Err err ->
+                    Debug.log (Debug.toString err)
+                        ( model, Cmd.none )
+
+        UpdateSlackInfo res ->
+            case res of
+                Ok slackInfo ->
+                    ( { model | slackInfo = slackInfo }, Cmd.none )
+
+                Err err ->
+                    Debug.log (Debug.toString err)
+                        ( model, Cmd.none )
+
+        UpdateLunchMenu res ->
+            case res of
+                Ok lunchMenu ->
+                    ( { model | lunchMenu = lunchMenu }, Cmd.none )
+
+                Err err ->
+                    Debug.log (Debug.toString err)
+                        ( model, Cmd.none )
+
+
+handleUrlRequest : Key -> UrlRequest -> Cmd Msg
+handleUrlRequest key urlRequest =
+    case urlRequest of
+        Internal url ->
+            Nav.pushUrl key (Url.toString url)
+
+        External url ->
+            Nav.load url
+
+
+httpErrorToString : Http.Error -> String
+httpErrorToString err =
+    case err of
+        BadUrl _ ->
+            "BadUrl"
+
+        Timeout ->
+            "Timeout"
+
+        NetworkError ->
+            "NetworkError"
+
+        BadStatus _ ->
+            "BadStatus"
+
+        BadBody s ->
+            "BadBody: " ++ s
+
+
+urlParser : Parser (Page -> msg) msg
+urlParser =
+    UrlParser.oneOf
+        [ UrlParser.map Transit <| UrlParser.s "transit"
+        , UrlParser.map Slack <| UrlParser.s "slack"
+        , UrlParser.map Birthday <| UrlParser.s "birthday"
+        , UrlParser.map Instagram <| UrlParser.s "instagram"
+        , UrlParser.map Weather <| UrlParser.s "weather"
+        , UrlParser.map Lunch <| UrlParser.s "lunch"
+        ]
+
+
+staticPages : List Page
+staticPages =
+    [ Transit
+    , Slack
+    , Birthday
+    , Instagram
+    , Weather
+    , Lunch
+    ]
+
+
+getActivePage : Page -> ( Int, Page )
+getActivePage page =
+    List.indexedMap pair staticPages
+        |> List.filter (\( _, page_ ) -> page == page_)
+        |> List.head
+        |> Maybe.withDefault ( 0, Transit )
+
+
+getActivePage_ : Int -> ( Int, Page )
+getActivePage_ index =
+    List.indexedMap pair staticPages
+        |> List.filter (\( index_, _ ) -> index == index_)
+        |> List.head
+        |> Maybe.withDefault ( 0, Transit )
+
+
+pages : List ( Int, Page )
+pages =
+    List.indexedMap pair staticPages
