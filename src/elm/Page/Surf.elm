532fe69cb1bd922582624e7d3e8edec858a5cb23
mimarz <mimarz@gmail.com>
2020-10-26 09:30:41 +0100

    with title now

--- src/elm/Page/Surf.elm
+++ src/elm/Page/Surf.elm
@@ -8,5 +8,14 @@ import Model exposing (..)
 view : Model -> Html Msg
 view _ =
     div [ class "page surf-page" ]
-        [ text "No surf today 🏄\u{1F3FB}\u{200D}♂️ 😢"
+        [ title
+        ]
+
+
+title : Html msg
+title =
+    div [ class "title" ]
+        [ div [ class "animated fadeInDown faster" ]
+            [ h1 [] [ text "No surf today 🌊🏄\u{1F3FB}\u{200D}♂️ 😢" ]
+            ]
         ]
